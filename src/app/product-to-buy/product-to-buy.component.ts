b9347da5482224a1f89ea27dee8e025e84ebf8be
Giorgio <vaccarino.giorgio@gmail.com>
2024-06-03 15:09:48 +0200

    aggiunto collegamento tra viste e badge articoli comprarti/da comprare con aggiornamento valore

--- src/app/product-to-buy/product-to-buy.component.ts
+++ src/app/product-to-buy/product-to-buy.component.ts
@@ -1,20 +1,68 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { Product } from '../model/Product';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-product-to-buy',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
+
+    <!-- Contatori -->
+    <div class="w-full md:w-2/5 mx-auto my-5">
+      <div class="flex justify-between">
+        <button class="btn">
+          Completati
+          <div class="badge">{{ completedProducts() }}</div>
+        </button>
+        <button class="btn">
+          Da Fare
+          <div class="badge badge-secondary">{{ toDoProducts() }}</div>
+        </button>
+      </div>
+    </div>
     <!-- Lista prodotti -->
     <div class="w-full md:w-2/5 mx-auto border border-neutral rounded-lg">
-      <label class="cursor-pointer label mr-4">
-        <span class="label-text">Prodotto 1</span>
-          <input type="checkbox" checked="checked" class="checkbox">
-      </label>
+      @for (product of products(); track product.id) {
+        <label class="cursor-pointer label mr-4">
+          <span class="label-text" [ngClass]="{'line-through text-accent': product.completed}">{{product.name}}</span>
+            <input type="checkbox" [checked]="product.completed" class="checkbox" (click)="onComplete(product)">
+        </label>
+      }
+      
     </div>
   `,
   styles: ``
 })
-export class ProductToBuyComponent {
+export class ProductToBuyComponent implements OnInit {
+  
+  http = inject(HttpClient);
+  url = 'http://localhost:3000/products';
+  products = signal<Product[]>([]);
 
+  completedProducts = computed(() => this.products().filter(product => product.completed).length);
+  toDoProducts = computed(() => this.products().filter(product => !product.completed).length);
+  
+    ngOnInit(): void {
+      this.http.get<Product[]>(this.url)
+        .subscribe(res => {
+          this.products.set(res.filter(product => product.added))
+        });
+    }
+
+  onComplete(item: Product) {
+    
+    let updateProduct = {...item, completed: !item.completed};
+
+    this.http.patch<Product>(`${this.url}/${item.id}`, updateProduct)
+     .subscribe(res =>{
+      console.log(res);
+        this.products.update(prev => {
+          return prev.map(product => product.id === item.id ? res : product)
+        })
+     })
+
+  }
+  
 }
